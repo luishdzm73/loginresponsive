e295383a0d6ea66567e74644885121783f388375
luis-hernandez10 <luishdzm73@gmail.com>
2023-05-15 13:09:39 -0600

    login resposivo alumno: Luis Hernandez Profesor: Roger Natividad Youtube

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Menú responsivo</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+<section class="left-form">
+	
+</section>
+
+<section class="right-form">
+	<form action="" method="post">
+		
+		<h2>Acceso login</h2>
+
+		<label for="usuario">Usuario:</label>
+		<input type="text" name="usuario" id="usuario">
+
+		<label for="usuario">Contraseña:</label>
+		<input type="text" name="usuario" id="usuario">
+
+		<div class="check">
+			<input type="checkbox" name="" id="">
+			<span>Registrar</span>
+		</div>
+
+		<input type="submit" value="Ingresar">
+
+	</form>
+</section>
+
+</body>
+</html>
